c710288d182fba297d16f46f13ab26c1b05acaab
fredtft <fredtft@gmail.com>
2026-02-08 19:22:10 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,17 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+// https://vitejs.dev/config/
+export default defineConfig({
+  // La proprietà base assicura che tutti i percorsi generati (JS, CSS, Immagini)
+  // inizino con il nome della sottocartella del progetto invece che dalla radice /.
+  base: '/AchilleCastiglioni/',
+  plugins: [react()],
+  build: {
+    // Assicuriamoci che la cartella di output sia quella standard
+    outDir: 'dist',
+    // Gli asset verranno messi in /AchilleCastiglioni/assets/
+    assetsDir: 'assets',
+  }
 });
